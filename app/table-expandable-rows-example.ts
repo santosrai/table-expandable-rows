94e690885391f6d8d5ca178ad3b2d428cb2dc5ef
santosh rai <santosh984970@gmail.com>
2019-03-29 04:38:09 +0000

    change

--- app/table-expandable-rows-example.ts
+++ app/table-expandable-rows-example.ts
@@ -22,38 +22,89 @@ export class TableExpandableRowsExample {
   expandedElement: PeriodicElement;
 }
 
-export interface Item {
-    sn: string;
-    nam: string;
+// export interface Item {
+//     sn: string;
+//     nam: string;
+// }
+// export interface PeriodicElement {
+//   name:{
+//       [key: string]: Item
+//       };
+//   position: number;
+//   weight: number;
+//   symbol: string;
+//   description: string;
+// }
+
+export interface Body_Size{
+  wrist_circ: number;
+  forearm_circ: number;
+  bicep_circ: number;
+  volume: number;
+  length: number;
+  
 }
-export interface PeriodicElement {
-  name:{
-       [key: string]: Item
+  
+export interface Biometric{
+  name: string;
+  value: number;
+}
+
+export interface myJsonData {
+  Body_Size_Measurements:{
+       [key: string]: Body_Size
       };
-  position: number;
-  weight: number;
-  symbol: string;
-  description: string;
+  Biometric_Data: {
+       [key: string]: Biometric
+      };
+ 
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 1,
-    name: [`h2`,`Hydrogen`],
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-  }, {
-    position: 2,
-    name: ['h','Helium'],
-    weight: 4.0026,
-    symbol: 'He',
-    description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {
+//     position: 1,
+//     name: [`h2`,`Hydrogen`],
+//     weight: 1.0079,
+//     symbol: 'H',
+//     description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+//         atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
+//   }, {
+//     position: 2,
+//     name: ['h','Helium'],
+//     weight: 4.0026,
+//     symbol: 'He',
+//     description: `Helium is a chemical element with symbol He and atomic number 2. It is a
+//         colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
+//         group in the periodic table. Its boiling point is the lowest among all the elements.`
 
-  },
+//   },
+// ];
+
+const ELEMENT_DATA: myJsonData[] = [
+  {
+    Body_Size_Measurements: 
+    [
+    {Right_Arm:[
+        wrist_circ: 10,
+        forearm_circ: 10,
+        bicep_circ: 11,
+        volume: 11,
+        length: 12]
+    },
+    {Left_Arm:[
+       wrist_circ: 10,
+        forearm_circ: 10,
+        bicep_circ: 11,
+        volume: 11,
+        length: 12]
+    }],
+    
+    Biometric_Data:[
+    { name: "Glucose Level", value: 100 },
+    { name: "HDL Cholesterol", value: 100 },
+    { name: "LDL Cholesterol", value: 200 }]
+   
+  }
 ];
 
 
